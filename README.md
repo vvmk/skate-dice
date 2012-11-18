e61dac970e932137f5a0ae734af3551ca5cab960
vincentmasiello <vincentmasiello@gmail.com>
2012-11-18 17:18:43 -0500

    first commit. add README, v0.1 functionality

---
+++ README.md
@@ -0,0 +1,23 @@
+## Skate Dice
+######version 0.1
+
+<br />
+This is a simple skate dice app designed for the web. Read below if you want to know why. Find it hosted [here](). (this link will work when it's actually hosted)
+
+####TODO:
+ - add separate player tracking
+ - show full trick title or possibilities (i.e. 360 + kickflip + backside == tre flip || backside 360 kickflip)
+ - ledge/rail & manual options
+ - difficulty selection for each dice individually
+ - exclude certain hard tricks on easy mode (reg bigspin heelflips still possible on easy for example o_O)
+ - make trick output smarter
+ - only fakie, switch, regular -> add 'ollie'
+	 - only  frontside/backside -> add '180' or 'ollie'
+ - move down settings into sexy 'prefs' pane; show/hide
+ - make everything prettier (dice graphics and such)
+
+#### Why?
+Neither I nor this project are affiliated with whichever entity owns the official 'SK8 Dice' brand of products in any way. This was written for fun. 
+
+I liked using the official SK8 dice app on my [smart phone] to warm up before a session, practice flatground tricks, etc. When my [smart phone] was stolen I had to activate an old flip-phone from 2005. Rather than keep asking others to download it for me on their [smart phone]s, i decided to design a simple web app they could use instead.  
+
---
+++ skate-dice.js
@@ -0,0 +1,84 @@
+//Skate Dice for web 
+//
+//version 0.1
+//
+//written by Vincent Masiello in about an hour on 11/18/2012 when he should have been working on an important deadline.
+//contact me: vincentmasiello@gmail.com
+//
+//Neither I nor this project are affiliated with whichever entity owns the official 'SK8 Dice' brand of products in any way. 
+//This was written for fun. My [smart phone] was stolen and i had to activate an old flip-phone from 2005 and didn't want to
+//ask my friend to buy an app for me on his [smart phone]. 
+//
+//
+//TODO: 
+	//add separate player tracking
+	//show full trick title or possibilities (i.e. 360 + kickflip + backside == tre flip || backside 360 kickflip)
+	//ledge and rail editions
+	//difficulty selection for each dice individually
+	//exclude certain hard tricks (reg bigspin heelflips still possible for example o_O)
+	//make trick output smarter
+		//only fakie, switch, regular -> add 'ollie'
+		//only  frontside/backside -> add '180' or 'ollie'
+	//move down settings into sexy 'prefs' pane; show/hide
+	//make everything prettier (dice graphics and such)
+
+//settings
+var difficulty = 6; //if easy, dilute dice with easier tricks
+var play_skate = false; //if false, 'x' and 'skate dice' will not be shown if rolled
+
+var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        init();
+        clearInterval(readyStateCheckInterval);
+    }
+}, 10);
+
+function init() {
+	play_skate = document.getElementById('skate_toggle').checked;
+	if (document.getElementById('radio_easy').checked == true)
+		difficulty = 12;
+};
+
+//Ayo, we just made you 12-sided DIE, mufucka!
+var d1 = ["regular", "fakie", "switch", "nollie", "x", "skate\ndice", "regular", "regular", "fakie", "fakie", "x", "skate\ndice"];
+var d4 = ["kickflip", "heelflip", "shuvit", "x", "x", "skate\ndice", "shuvit", "shuvit", "x", "skate\ndice", "x", "skate\ndice"];
+var d3 = ["180", "180", "360", "bigspin", "x", "skate\ndice", "180", "bigspin", "x", "skate\ndice", "x", "skate\ndice"];
+var d2 = ["frontside", "backside", "frontside", "backside", "x", "skate\ndice", "x", "skate\ndice", "x", "skate\ndice", "x", "skate\ndice"];
+
+var die = [d1, d2, d3, d4];
+
+function roll() {
+	var output = '';
+	var extra = [0, 0]; //[0] = 'x', [1] = 'skate\ndice'
+	for (var x=0; x<die.length;x++) {
+		var o;
+		var rand = Math.floor(Math.random()*difficulty);
+		o = die[x][rand];
+		
+		console.log(o);
+		if (o === 'x' || o === 'skate\ndice') {
+			var i = (o === 'x') ? 0 : 1;
+			extra[i]++;
+		} else {
+			output += (' '+o);
+		}
+	}
+	if (output==='')
+		output = 'Pick your own!';
+		
+	document.getElementById('dice_container').innerHTML = '<br />'+output;
+	document.getElementById('extra_container').innerHTML = (play_skate) ? '<br /><span>X: '+extra[0]+'</span><br /><span>Skate Dice: '+extra[1]+'</span>' : '';
+}
+
+function changeDifficulty(diff) {
+	difficulty = diff;
+}
+
+function toggleSkate() {
+	var cb = document.getElementById('skate_toggle');
+	if (cb.checked == true) {
+		play_skate = true;
+	} else {
+		play_skate = false;
+	}
+}
